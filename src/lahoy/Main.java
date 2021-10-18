e13954d7c007475fe366e6ef843c9010cbd8e3c3
bartm <justkoller@hotmail.com>
2021-10-18 11:56:38 -0600

    Added hello line for justin

--- src/lahoy/Main.java
+++ src/lahoy/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello, my name is Alyssa.");
         System.out.println("Hello Alyssa, my name is Pierre.");
+        System.out.println("Hello Alyssa, my name is Justin.");
     }
 }
